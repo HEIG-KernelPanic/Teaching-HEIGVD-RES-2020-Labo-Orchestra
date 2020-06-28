441c092064359c32f8d00efda6da02040ff8c5b2
ClarFleu <clarisse.fleu@gmail.com>
2020-06-28 09:37:06 +0200

    Add instruments for the musicians

---
+++ docker/image-musician/src/global.js
@@ -0,0 +1,12 @@
+// Network infos
+exports.multicastAddress = "239.1.1.100"
+exports.port             = 9009
+exports.interval         = 1000
+
+// Instruments
+exports.instruments = new Map();
+exports.instruments["piano"]   = "ti-ta-ti";
+exports.instruments["trumpet"] = "pouet";
+exports.instruments["flute"]   = "trulu";
+exports.instruments["violin"]  = "gzi-gzi";
+exports.instruments["drum"]    = "boum-boum";
--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,12 +1,37 @@
+// Musician application
+
 const dgram = require('dgram');
 
+// Import global variables
+const global = require('./global.js');
+
 const server = dgram.createSocket('udp4');
+
+const instrument = process.argv[2];
 
 server.setBroadcast(true);
 
 function sendMusic() {
-	console.log("Sending my music");
-	server.send("Music", port=8080, address='255.255.255.255');
+	console.log("Playing the " + instrument + " : " + global.instruments[instrument]);
+
+	const payload = JSON.stringify({
+		"instrument" : instrument,
+        "sound"      : global.instruments[instrument],
+        "uuid"       : uuidv4
+    });
+
+    server.send(payload, payload.length, global.port, global.multicastAddress, function() {
+        console.log("Sent " + payload);
+    });
+
 }
 
-setInterval(sendMusic, 1000);
+if (! global.interval.has(instrument)) {
+    console.log('I don\'t know this instrument...');
+    process.exit(1);
+} else {
+	setInterval(sendMusic, global.interval);
+}
+
+server.bind(global.port);
+
